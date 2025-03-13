a029809024bb5cba8b5ee2bcd60d7c68985b69f6
suba-devi-21 <subadevirajasekaran@gmail.com>
2025-03-13 12:56:16 +0530

    commit

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ const cors = require('cors');
 require("dotenv").config();
 const bodyParser = require('body-parser');
 const { connectMongoDB } = require("./Database/db");
+const postRouter = require("./ModelsAndControllers/postController");
+const userRouter = require("./ModelsAndControllers/userController");
+
 const PORT = process.env.PORT;
 const HOSTNAME = process.env.HOSTNAME;
 const app = express();
 connectMongoDB();
 app.use(bodyParser.json());
 
-app.use(cors({origin:"https://aditith.netlify.app",credentials: true}))
+app.use(cors({origin:"https://frontend-final-lilac-iota.vercel.app/",credentials: true}))
 
-
+//App Routes
+app.use("/user", userRouter);
+app.use("/post", postRouter);
 
 app.get("/", (req, res) => {
   res.send("Server running successfully!!!");
 });
-app.use('/user',require('./ModelsAndControllers/userController'));
-app.use('/post',require('./ModelsAndControllers/postController'));
+
 
 app.listen(PORT, HOSTNAME, () => {
   console.log(`Server Running Successfully at ${PORT}`);
