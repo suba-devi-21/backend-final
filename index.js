6957b8e0cdc85608d5363c8f4875b4cdc70b1d5d
suba-devi-21 <subadevirajasekaran@gmail.com>
2025-03-13 13:19:54 +0530

    commit

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const { connectMongoDB } = require("./Database/db");
 const postRouter = require("./ModelsAndControllers/postController");
 const userRouter = require("./ModelsAndControllers/userController");
-
 const PORT = process.env.PORT;
+
 const app = express();
 connectMongoDB();
+
 app.use(bodyParser.json());
 
-app.use(cors({origin:"*",credentials: true}))
-
-//App Routes
-app.use("/user", userRouter);
-app.use("/post", postRouter);
+app.use(cors({ origin:"", credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("Server running successfully!!!");
 });
+//App Routes
+app.use("/user", userRouter);
+app.use("/post", postRouter);
 
-
-app.listen(PORT,  () => {
-  console.log(`Server Running Successfully at ${PORT}`);
+app.listen(4000, () => {
+  console.log(`Server Running Successfully at 4000`);
 });
